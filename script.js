d247b5efe591dc0518b8f18d0203c172485973c8
CharanMN7_pc <maverickid247@gmail.com>
2024-03-04 17:32:49 +0530

    Added randomButton variable, added comments, updated HTML

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// search functionality + random
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const randomButton = document.getElementById("random-pokemon");
+// detials card
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const types = document.getElementById("types");
+// pokemon sprite
+const sprite = document.getElementById("sprite");
+// stats
 const hp = document.getElementById("hp");
 const attack = document.getElementById("attack");
 const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
-const sprite = document.getElementById("sprite");
