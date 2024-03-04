8e1a0d643c81a04cdf69ddfd1fab1c49e6bdecc1
CharanMN7_pc <maverickid247@gmail.com>
2024-03-04 20:11:29 +0530

    Added a bunch of spaces

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 /* Constants */
+
 // search functionality + random
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const randomButton = document.getElementById("random-pokemon");
+
 // detials card
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const types = document.getElementById("types");
+
 // pokemon sprite
 const sprite = document.getElementById("sprite");
+
 // stats
 const hp = document.getElementById("hp");
 const attack = document.getElementById("attack");
@@ -18,10 +22,12 @@ const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+
 // api
 const api = "https://pokeapi.co/api/v2/pokemon/";
 
 /* Fucntions */
+
 // returns an abject containing all the required fields.
 const getData = async (query) => {
   const res = await fetch(api + query);
